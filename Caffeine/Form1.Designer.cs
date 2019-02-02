a5e8aa563c1a97fd348bbd58c9616a8f9cb8f9e4
Andrew Potts <AndrewPotts@asos.com>
2019-02-02 14:12:46 +0000

    First commit

---
+++ Caffeine/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Caffeine
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timeout = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(279, 31);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 38);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "&Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(279, 75);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 34);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "S&top";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timeout
+            // 
+            this.timeout.AutoSize = true;
+            this.timeout.Location = new System.Drawing.Point(32, 48);
+            this.timeout.Name = "timeout";
+            this.timeout.Size = new System.Drawing.Size(82, 20);
+            this.timeout.TabIndex = 2;
+            this.timeout.Text = "Loading ...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 144);
+            this.Controls.Add(this.timeout);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Caffeine";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer mainTimer;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label timeout;
+    }
+}
+
---
+++ Caffeine/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caffeine
+{
+    public partial class MainForm : Form
+    {
+        [Flags]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_CONTINUOUS = 0x80000000
+        }
+        public SYSTEMTIMEOUTS TimeOuts => sysTimeouts;
+
+        public struct SYSTEMTIMEOUTS
+        {
+            public int BATTERYIDLETIMEOUT;
+            public int EXTERNALIDLETIMEOUT;
+            public int WAKEUPIDLETIMEOUT;
+        }
+
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE flags);
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SystemParametersInfo")]
+        internal static extern int SystemParametersInfo(int uiAction, int uiParam, ref int pvParam, int fWinIni);
+
+        private static System.Threading.Timer preventSleepTimer = null;
+        public const int SPI_GETBATTERYIDLETIMEOUT = 252;
+        public const int SPI_GETEXTERNALIDLETIMEOUT = 254;
+        public const int SPI_GETWAKEUPIDLETIMEOUT = 256;
+        public static int timeOutinMS = 0;
+        public static int batteryIdleTimer;
+        public static int externalIdleTimer;
+        public static int wakeupIdleTimer;
+        public static SYSTEMTIMEOUTS sysTimeouts;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            startButton_Click(sender, e);
+        }
+
+        public static void GetSystemTimeOuts()
+        {
+            sysTimeouts.BATTERYIDLETIMEOUT = -2;
+            sysTimeouts.EXTERNALIDLETIMEOUT = -2;
+            sysTimeouts.WAKEUPIDLETIMEOUT = -2;
+
+            if (SystemParametersInfo(SPI_GETBATTERYIDLETIMEOUT, 0, ref batteryIdleTimer, 0) == 1)
+                sysTimeouts.BATTERYIDLETIMEOUT = batteryIdleTimer;
+            else
+                sysTimeouts.BATTERYIDLETIMEOUT = -1;
+
+            if (SystemParametersInfo(SPI_GETEXTERNALIDLETIMEOUT, 0, ref externalIdleTimer, 0) == 1)
+                sysTimeouts.EXTERNALIDLETIMEOUT = externalIdleTimer;
+            else
+                sysTimeouts.EXTERNALIDLETIMEOUT = -1;
+
+            if (SystemParametersInfo(SPI_GETWAKEUPIDLETIMEOUT, 0, ref wakeupIdleTimer, 0) == 1)
+                sysTimeouts.WAKEUPIDLETIMEOUT = wakeupIdleTimer;
+            else
+                sysTimeouts.WAKEUPIDLETIMEOUT = -1;
+
+
+            if (timeOutinMS < sysTimeouts.BATTERYIDLETIMEOUT)
+                timeOutinMS = sysTimeouts.BATTERYIDLETIMEOUT;
+            if (timeOutinMS < sysTimeouts.EXTERNALIDLETIMEOUT)
+                timeOutinMS = sysTimeouts.EXTERNALIDLETIMEOUT;
+            if (timeOutinMS < sysTimeouts.WAKEUPIDLETIMEOUT)
+                timeOutinMS = sysTimeouts.WAKEUPIDLETIMEOUT;
+
+            if (timeOutinMS == 0)
+                timeOutinMS = 30;
+        }
+
+        public void DisableDeviceSleep()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+            mainTimer.Interval = timeOutinMS*1000;
+            mainTimer.Start();
+        }
+
+        public static void EnableDeviceSleep()
+        {
+            preventSleepTimer.Dispose();
+            preventSleepTimer = null;
+        }
+
+        private static void PokeDeviceToKeepAwake()
+        {
+            try
+            {
+                SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+                var handle = FindWindow("SysListView32", "FolderView");
+
+                if (handle == IntPtr.Zero) return;
+                SetForegroundWindow(handle);
+                SendKeys.SendWait("%1");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        { 
+            PokeDeviceToKeepAwake();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = true;
+            startButton.Enabled = false;
+            GetSystemTimeOuts();
+            timeout.Text = $"{timeOutinMS} seconds";
+            DisableDeviceSleep();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+            mainTimer.Stop();
+            timeout.Text = @"Caffeine suspended";
+        }
+    }
+}
